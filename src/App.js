e0ca4f4d1fb27a8af94a8ed01a9bb812aa6fc7e9
Kyaw Thet Khaing <kyawthet.khaing@yomabank.com>
2020-08-22 00:29:32 +0630

    markdown previewer all tests passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import marked from "marked";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+marked.setOptions({
+  breaks: true
+});
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "# Welcome to my React Markdown Previewer!"
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  createMarkUp = () => {
+    marked.setOptions({
+      breaks: true
+    });
+    let rawMarkup = marked(this.state.input);
+
+    return { __html: rawMarkup };
+  };
+
+  render() {
+    return (
+      <div className="markdown container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Editor</h4>
+            <textarea
+              id="editor"
+              className="textarea"
+              value={this.state.input}
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+          <div className="col-md-6">
+            <h4>Markdown</h4>
+            <span
+              id="preview"
+              dangerouslySetInnerHTML={this.createMarkUp()}
+            ></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
